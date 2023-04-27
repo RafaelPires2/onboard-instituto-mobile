14cd7a0dcd3fe5c8a239dbeb9235de50de9dd3b3
rafaelpires2 <rafaelbrpires@gmail.com>
2023-04-27 11:24:30 -0300

    feature: routes navigation

--- App.js
+++ App.js
@@ -5,17 +5,26 @@ import { ThemeProvider } from 'styled-components/native';
 import { StatusBar } from 'expo-status-bar';
 import { KeyboardAvoidingView, StyleSheet } from 'react-native';
 import { MyTheme } from './src/styles/themes/default';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 const client = new ApolloClient({
   uri: 'https://template-onboarding-node-sjz6wnaoia-uc.a.run.app/graphql',
   cache: new InMemoryCache(),
 });
 
+const Stack = createNativeStackNavigator();
+
 export default function App() {
   return (
     <ApolloProvider client={client}>
       <ThemeProvider theme={MyTheme}>
         <KeyboardAvoidingView style={styles.containerKeyBoard}>
+          <NavigationContainer initialRouteName="Login">
+            <Stack.Navigator>
+              <Stack.Screen name="Login" component={Login} />
+            </Stack.Navigator>
+          </NavigationContainer>
           <Login />
           <StatusBar style="auto" />
         </KeyboardAvoidingView>
