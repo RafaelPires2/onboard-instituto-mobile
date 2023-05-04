e9d8a61f0a76373b443ac4171a8a8845ea4c0fe6
RafaelPires2 <rafaelbrpires@gmail.com>
2023-05-04 15:18:24 -0300

    feature: input options

--- src/components/input/styles.ts
+++ src/components/input/styles.ts
@@ -1,12 +1,16 @@
 import styled from 'styled-components/native';
 import { widthPercentageToDP as wp } from 'react-native-responsive-screen';
 
-export const WrapperInput = styled.View``;
+export const WrapperInput = styled.View`
+  display: flex;
+  gap: 8px;
+  margin-bottom: 16px;
+`;
 
 export const CustomLabel = styled.Text`
   font-size: 12px;
-  margin-bottom: 8px;
   color: #f1f1f2;
+  width: ${wp('80%')}px;
 `;
 
 export const Input = styled.TextInput`
@@ -14,7 +18,6 @@ export const Input = styled.TextInput`
   border-radius: 6px;
   padding: 10px;
   font-size: 16px;
-  margin-bottom: 12px;
   width: ${wp('80%')}px;
   height: 47px;
 `;
@@ -22,4 +25,14 @@ export const Input = styled.TextInput`
 export const TextError = styled.Text`
   font-size: 14px;
   color: red;
+`;
+
+export const WrapperInputOption = styled.View`
+  background-color: ${({ theme }: any) => theme.COLORS.BG_SECONDARY};
+  border-radius: 6px;
+  font-size: 16px;
+  width: ${wp('80%')}px;
+  color: #f1f1f2;
+  margin-top: 8px;
+  margin-bottom: 26px;
 `;
